bde4aed6bb79ebc7dc9b3f849ec0f44b0b4e63d2
guadamedel <guadamepet2@gmail.com~>
2025-09-01 20:07:10 -0300

    tp8 JS

---
+++ TPn°8_JavaScript/Eje2_JavaScript3ConJS.js
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <script type="text/javascript">
+  
+      document.write("Hola Mundo");
+    </script>
+  </head>
+  <body>
+    <noscript>
+      El browser no tiene habilitada la ejecución de código javascript
+    </noscript>
+  </body>
+</html>
